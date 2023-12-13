ee6a415b2a956a782b1435a83e9ae3ae8d1e8946
jameswmccarty <jameswmccarty@gmail.com>
2023-12-12 19:59:44 -0500

    Update Day 12 (Part 2)

--- day12.py
+++ day12.py
@@ -102,6 +102,10 @@ After unfolding, adding all of the possible arrangement counts together produces
 
 Unfold your condition records; what is the new sum of possible arrangement counts?
 
+Your puzzle answer was 17485169859432.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 from collections import deque
@@ -124,7 +128,34 @@ def ways(s,n):
 			if len(s) > 0 and s[0] != '#':
 				q.append((s[1:],n[:],b+'.'))
 	return sols				
-	
+
+seen = dict()
+
+def ways2(s,n):
+	if (s,tuple(n)) in seen:
+		return seen[(s,tuple(n))]
+	if len(n) == 0 and (len(s) == 0 or '#' not in set(s)):
+		return 1
+	if len(n) > 0 and len(s) >= n[0]:
+			total = 0
+			if '.' not in s[0:n[0]]:
+				if len(s) == n[0]:
+					total += ways2(s[n[0]:],n[1:])
+				elif s[n[0]] != '#':
+					total += ways2(s[n[0]+1:],n[1:])
+			if len(s) > 0 and s[0] != '#':
+				total += ways2(s[1:],n[:])
+			seen[(s,tuple(n))] = total
+			return total
+	return 0
+
+def unfold(s,n):
+	out_s = s[:]
+	out_n = n[:]
+	for _ in range(4):
+		out_s += '?' + s
+		out_n += n[:]
+	return out_s,out_n
 
 if __name__ == "__main__":
 
@@ -137,4 +168,12 @@ if __name__ == "__main__":
 			total += len(ways(s,n))
 	print(total)
 
+	total = 0
 	# Part 2 Solution
+	with open("day12_input", "r") as infile:
+		for line in infile:
+			s,n = line.strip().split(' ')
+			n = [ int(x) for x in n.split(',') ]
+			ns,nn = unfold(s,n)
+			total += ways2(ns,nn)
+	print(total)
