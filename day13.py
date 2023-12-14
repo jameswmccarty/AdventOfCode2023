b5eb6d09814efca9014f62032aa913139e0a7fa9
James McCarty <jameswmccarty@gmail.com>
2023-12-13 20:25:42 -0500

    Update Day 13 (Part 2)

--- day13.py
+++ day13.py
@@ -123,6 +123,10 @@ Summarize your notes as before, but instead use the new different reflection lin
 
 In each pattern, fix the smudge and find the different line of reflection. What number do you get after summarizing the new reflection line in each pattern in your notes?
 
+Your puzzle answer was 30442.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 def reflect(lines):
@@ -140,6 +144,26 @@ def reflect(lines):
 			return i
 	return 0
 
+def diff(a,b):
+	count = 0
+	for i,c in enumerate(a):
+		if b[i] != c:
+			count += 1
+	return count
+
+def reflect2(lines):
+	for i in range(1,len(lines)):
+		count = 0
+		l = i-1
+		r = i
+		while l >= 0 and r < len(lines):
+			count += diff(lines[l],lines[r])
+			l -= 1
+			r += 1
+		if count == 1:
+			return i
+	return 0
+
 def score(block):
 	total = 0
 	h_lines = block.split('\n')
@@ -150,12 +174,28 @@ def score(block):
 		v_lines.append(v_line)
 	return total + reflect(v_lines)
 
+def score2(block):
+	h_lines = block.split('\n')
+	v_lines = list()
+	for i in range(len(h_lines[0].strip())):
+		v_line = ''.join( x[i] for x in h_lines )
+		v_lines.append(v_line)
+	v_total = reflect2(v_lines)
+	if v_total > 0:
+		return v_total
+	total = 100*reflect2(h_lines)
+	if total > 0:
+		return total
+	return 0
+
 if __name__ == "__main__":
 
 	# Part 1 Solution
-
 	with open("day13_input", "r") as infile:
 		blocks = infile.read().strip().split('\n\n')
 	print(sum(score(b) for b in blocks))
+	
+	# Part 2 Solution
+	print(sum(score2(b) for b in blocks))
 
 
