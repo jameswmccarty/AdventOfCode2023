b98684bf0041b0b0b626e93bd10c17fd2dc2aeb0
James McCarty <jameswmccarty@gmail.com>
2023-12-15 16:37:23 -0500

    Update (Part 2)

--- day15.py
+++ day15.py
@@ -165,6 +165,9 @@ So, the above example ends up with a total focusing power of 145.
 
 With the help of an over-enthusiastic reindeer in a hard hat, follow the initialization sequence. What is the focusing power of the resulting lens configuration?
 
+Your puzzle answer was 244461.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
 
 """
 
@@ -183,4 +186,35 @@ if __name__ == "__main__":
 		print(sum(h(x) for x in infile.read().strip().split(',')))
 
 	# Part 2 Solution
+	with open("day15_input", "r") as infile:
+		instructions = infile.read().strip().split(',')
 
+	hm = dict()
+	for i in range(256):
+		hm[i] = []
+
+	for e in instructions:
+		if '-' in e:
+			l = e.replace('-','')
+			i = h(l)
+			for j in hm[i]:
+				if j.split(' ')[0] == l:
+					hm[i].remove(j)
+					break
+		else:
+			l,v = e.split('=')
+			i = h(l)
+			r = -1
+			for j,k in enumerate(hm[i]):
+				if k.split(' ')[0] == l:
+					r = j
+					break
+			if r == -1:
+				hm[i].append(l+' '+v)
+			else:
+				hm[i][r] = l+' '+v
+	total = 0
+	for i in range(256):
+		for j,v in enumerate(hm[i]):
+			total += (i+1)*(j+1)*int(v.split(' ')[1])
+	print(total)
