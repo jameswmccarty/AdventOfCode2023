bd79c9add99f5ec462bcb8d83ece67d77860e814
James McCarty <jameswmccarty@gmail.com>
2023-12-17 10:17:32 -0500

    Update (Part 2)

--- day17.py
+++ day17.py
@@ -104,6 +104,10 @@ This route causes the ultra crucible to incur the minimum possible heat loss of 
 
 Directing the ultra crucible from the lava pool to the machine parts factory, what is the least heat loss it can incur?
 
+Your puzzle answer was 918.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 import heapq
@@ -140,6 +144,35 @@ def search(dest,layout):
 						heapq.heappush(q,(loss+layout[(nx,ny)],(nx,ny),(dx,dy),steps+1))
 	return float('inf')
 
+def search2(dest,layout):
+	pos = (0,0) # begin upper left
+	q = []
+	seen = set()
+	heapq.heapify(q)
+	# heat lost, pos, direction, steps_before_turn, steps_taken
+	heapq.heappush(q,(0,pos,(1,0),3,0))
+	heapq.heappush(q,(0,pos,(0,-1),3,0))
+	while q:
+		loss,pos,direction,steps_before_turn,steps_taken = heapq.heappop(q)
+		if pos == dest and steps_before_turn == 0 and steps_taken <= 10:
+			return loss
+		if hash((pos,direction,steps_before_turn,steps_taken)) not in seen:
+			seen.add(hash((pos,direction,steps_before_turn,steps_taken)))
+			next_dirs = set(deltas[direction])
+			if steps_before_turn > 0:
+				next_dirs = { direction }
+			if steps_taken == 10:
+				next_dirs.discard(direction)
+			x,y = pos
+			for dx,dy in next_dirs:
+				nx,ny = x+dx,y+dy
+				if (nx,ny) in layout:
+					if (dx,dy) != direction:
+						heapq.heappush(q,(loss+layout[(nx,ny)],(nx,ny),(dx,dy),3,1))
+					else:
+						heapq.heappush(q,(loss+layout[(nx,ny)],(nx,ny),(dx,dy),max(0,steps_before_turn-1),steps_taken+1))
+	return float('inf')
+
 if __name__ == "__main__":
 
 	# Part 1 Solution
@@ -155,4 +188,5 @@ if __name__ == "__main__":
 	print(search((x_dim-1,y-1),layout))
 
 	# Part 2 Solution
+	print(search2((x_dim-1,y-1),layout))
 
