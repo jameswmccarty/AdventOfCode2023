cd2f1757e4e02c53f55d577c61c25e3b1d942cda
James McCarty <jameswmccarty@gmail.com>
2023-12-19 21:19:38 -0500

    Update Day 19 (Part 2)

--- day19.py
+++ day19.py
@@ -73,6 +73,10 @@ In the above example, there are 167409079868000 distinct combinations of ratings
 
 Consider only your list of workflows; the list of part ratings that the Elves wanted you to sort is no longer relevant. How many distinct combinations of ratings will be accepted by the Elves' workflows?
 
+Your puzzle answer was 132392981697081.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 ruleset = dict()
@@ -96,6 +100,65 @@ def evaluate(part,rule):
 		if op == 'lt':
 			if part[key] < value:
 				return evaluate(part,dest)
+
+total = 0
+def traverse_count(rule,x,m,a,s):
+	global total
+	if rule == 'A':
+		total += (x[1]+1-x[0])*(m[1]+1-m[0])*(a[1]+1-a[0])*(s[1]+1-s[0])
+		return
+	if rule == 'R':
+		return
+	for criteria in ruleset[rule]:
+		if criteria == 'A':
+			total += (x[1]+1-x[0])*(m[1]+1-m[0])*(a[1]+1-a[0])*(s[1]+1-s[0])
+			return
+		if type(criteria) == type(''):
+			traverse_count(criteria,x,m,a,s)
+		if type(criteria) == type(()):
+			key,op,value,dest = criteria
+			if op == 'gt':
+				if key == 'x':
+					lo = x[0]
+					x = (value+1,x[1])
+					traverse_count(dest,x,m,a,s)
+					x = (lo,value)
+				if key == 'm':
+					lo = m[0]
+					m = (value+1,m[1])
+					traverse_count(dest,x,m,a,s)
+					m = (lo,value)
+				if key == 'a':
+					lo = a[0]
+					a = (value+1,a[1])
+					traverse_count(dest,x,m,a,s)
+					a = (lo,value)
+				if key == 's':
+					lo = s[0]
+					s = (value+1,s[1])
+					traverse_count(dest,x,m,a,s)
+					s = (lo,value)
+			elif op == 'lt':
+				if key == 'x':
+					hi = x[1]
+					x = (x[0],value-1)
+					traverse_count(dest,x,m,a,s)
+					x = (value,hi)
+				if key == 'm':
+					hi = m[1]
+					m = (m[0],value-1)
+					traverse_count(dest,x,m,a,s)
+					m = (value,hi)
+				if key == 'a':
+					hi = a[1]
+					a = (a[0],value-1)
+					traverse_count(dest,x,m,a,s)
+					a = (value,hi)
+				if key == 's':
+					hi = s[1]
+					s = (s[0],value-1)
+					traverse_count(dest,x,m,a,s)
+					s = (value,hi)
 
 if __name__ == "__main__":
 
@@ -140,4 +203,7 @@ if __name__ == "__main__":
 
 
 	# Part 2 Solution
+	total = 0
+	traverse_count('in',(1,4000),(1,4000),(1,4000),(1,4000))
+	print(total)
 
