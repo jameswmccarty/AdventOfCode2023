88d6948da665a05a6af96d161d860210e882d5c0
jameswmccarty <jameswmccarty@gmail.com>
2023-12-23 08:45:30 -0500

    Update Day 20 (Part 2)

--- day20.py
+++ day20.py
@@ -123,12 +123,23 @@ The final machine responsible for moving the sand down to Island Island has a mo
 
 Reset all modules to their default states. Waiting for all pulses to be fully handled after each button press, what is the fewest number of button presses required to deliver a single low pulse to the module named rx?
 
+Your puzzle answer was 243081086866483.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
+
 """
 
 from collections import deque
+import math
 
 high_sent = 0
 low_sent  = 0
+
+cycle_counters = { 'pv' : 0,
+		   'vv' : 0,
+		   'mr' : 0,
+		   'bl' : 0}
+cycle_count = 0
 
 class Button:
 
@@ -271,8 +282,10 @@ class Conjunction:
 		self.inputs[module] = '-low'
 
 	def tick(self):
-		global low_sent,high_sent
+		global low_sent,high_sent,cycle_counters
 		if all( value == '-high' for value in self.inputs.values() ):
+			if self.get_name() in cycle_counters and cycle_counters[self.get_name()] == 0:
+				cycle_counters[self.get_name()] = cycle_count
 			for e in self.outputs:
 				e.recv('-low',self)
 				low_sent += 1
@@ -345,4 +358,49 @@ if __name__ == "__main__":
 	print(low_sent*high_sent)
 
 	# Part 2 Solution
+	Modules = dict()
+	low_sent = 0
+	high_sent = 0
 
+	# Part 1 Solution
+	with open("day20_input", "r") as infile:
+		for line in infile:
+			module = gen_module_pass1(line)
+			Modules[module.get_name()] = module
+
+	#if 'output' not in Modules:
+	#	Modules['output'] = Output('output')
+
+	with open("day20_input", "r") as infile:
+		for line in infile:
+			source,dest = line.strip().split(' -> ')
+			for entry in dest.split(', '):
+				if entry not in Modules:
+					Modules[entry] = Output(entry)
+			if source == 'broadcaster':
+				for entry in dest.split(', '):
+					Modules[source].add_output(Modules[entry])
+			if source == 'button':
+				for entry in dest.split(', '):
+					Modules[source].add_output(Modules[entry])
+			if '%' in source:
+				for entry in dest.split(', '):
+					Modules[source[1:]].add_output(Modules[entry])
+			if '&' in source:
+				for entry in dest.split(', '):
+					Modules[source[1:]].add_output(Modules[entry])
+			for entry in dest.split(', '):
+				if Modules[entry].kind() == 'conjunction':
+					source_name = source[1:] if '%' in source or '&' in source else source
+					Modules[entry].add_input(Modules[source_name])
+
+	while True:
+		cycle_count += 1
+		Modules['button'].press()
+		while any( v.tick() if v.kind() != 'conjunction' else False for k,v in Modules.items() ):
+			continue
+		if all( x != 0 for x in cycle_counters.values() ):
+			print(math.lcm(*cycle_counters.values()))
+			exit()
+		
+	
